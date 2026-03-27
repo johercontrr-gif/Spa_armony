e82ba1e7fda5cd146b817a97659b4517466d51ff
lunacamilo2007-wq <lunacamilo2007@gmail.com>
2026-03-27 16:53:46 -0500

    crear las tablas de la db en migraciones

--- database/migrations/2026_03_27_201716_create_masajistas_table.php
+++ database/migrations/2026_03_27_201716_create_masajistas_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up()    {
         Schema::create('masajistas', function (Blueprint $table) {
-            $table->id();
+            $table->integer('cedula')->primary();
+            $table->string('nombre_masajista', 255);
+            $table->string('telefono', 15);
             $table->timestamps();
-        });
-    }
+        });    }
 
     /**
      * Reverse the migrations.
